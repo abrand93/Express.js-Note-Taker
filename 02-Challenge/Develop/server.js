4ad5ff73088cff65bac570fb3d363c115e5c4309
Verbal Kent <abrandstrom93@gmail.com>
2023-05-21 17:30:19 -0500

    added the fs and created the post to save new note

--- 02-Challenge/Develop/server.js
+++ 02-Challenge/Develop/server.js
@@ -1,19 +1,56 @@
 const express = require('express')
-const notes = require('./db/db.json')
+const notesdb = require('./db/db.json')
 const app = express()
  const PORT = 3001
+ const path = require('path')
+ const fs = require('fs')
 
-app.get('/api', (req, res) =>{
-    res.json(notes)
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+ app.use(express.static('public'))
+
+app.get('/', (req, res) =>{
+  res.sendFile(path.join(__dirname,'./public/index.html'))
+});
+app.get('/notes', (req, res) =>{
+  res.sendFile(path.join(__dirname,'./public/notes.html'))
+});
+
+
+
+app.get('/api/notes', (req, res) =>{
+
+     res.json(notesdb)
 })
 
-app.post('/api', (req, res) =>{
+app.post('/api/notes', (req,res) =>{
+const {title, text} = req.body
+if(title && text){
+const newNote = {
+  title,
+  text,
+}
+fs.readFile('./db/db.json', 'utf-8',(err,data) =>{
+  if (err){console.error(err)
+  } else{
+    const parsedNote = JSON.parse(data)
+  
+ 
 
-    res.json(`${req.method} was received`)
+  parsedNote.push(newNote)
+
+const noteString = JSON.stringify(newNote)
+
+fs.writeFile('./db/db.json', JSON.stringify(parsedNote,null, 4), (err) => err ? console.error(err): console.log("It worked "))
+}
+})
+}
 })
 
 
 
 
 
-app.listen(PORT,() => console.log(`server listening on localhost:${PORT}`))
+app.listen(PORT,() => console.log(`server listening on http://localhost:${PORT}`))
