3fa42fdf91df72ac5cc0a81bd51c74036dec9a89
Verbal Kent <abrandstrom93@gmail.com>
2023-05-22 17:05:34 -0500

    changed port to PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const notesdb = require('./db/db.json')
 const app = express()
- const PORT = process.env.port || 3001
+ const PORT = process.env.PORT || 3001
  const path = require('path')
  const fs = require('fs')
  const uniqid = require('uniqid')
@@ -12,15 +12,19 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
  app.use(express.static('public'))
 
-app.get('*', (req, res) =>{
-  res.sendFile(path.join(__dirname,'./public/index.html'))
-});
+
 app.get('/', (req, res) =>{
   res.sendFile(path.join(__dirname,'./public/index.html'))
 });
+
+// app.get('*', (req, res) =>{
+//   res.sendFile(path.join(__dirname,'./public/index.html'))
+// });
+
 app.get('/notes', (req, res) =>{
   res.sendFile(path.join(__dirname,'./public/notes.html'))
 });
+
 
 
 
