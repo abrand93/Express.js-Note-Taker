f88e4a4ebfc030466fd1663c601cd49498412f6e
Verbal Kent <abrandstrom93@gmail.com>
2023-05-23 18:02:11 -0500

    remove files

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended: true}))
 
 
 app.get('/', (req, res) =>{
-  res.sendFile(path.join(__dirname,'./public/index.html'))
+  res.sendFile(path.join(__dirname,'/public/index.html'))
 });
 
 // app.get('*', (req, res) =>{
